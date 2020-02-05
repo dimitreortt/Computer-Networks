0af6bfb2085e845de5514e67293ed407f55557e5
201519040032 <dimiortt@gmail.com>
2020-02-05 08:09:03 -0400

    Rename

---
+++ Trabalho 2/README.md
@@ -0,0 +1,15 @@
+This is the implementation of a Proxy HTTP,
+which is inserted between every HTTP Request 
+from a browser aplication and its responses.
+It only treats GET Requests while the others
+work normally.
+
+OVERVIEW: Header.h
+
+Declaration of libraries used 
+in the implementation of the
+proxy HTTP.
+
+Last revision: 09/04/2019
+Author(s): Dimitre Ortt and Jorge Emerson
+===
--- Trabalho 2/header.h
+++ Trabalho 2/header.h
@@ -1,3 +1,14 @@
+//
+// OVERVIEW: Header.h
+// ========
+// Declaration of libraries used 
+// in the implementation of the
+// proxy HTTP.
+//
+// Author(s): Dimitre Ortt and Jorge Emerson
+// Last revision: 09/04/2019
+
+
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -8,19 +19,16 @@
 #include <netinet/in.h>
 #include <pthread.h>
 #include <netdb.h>
-
-void *dialogue(void *args);
-char * get_url(char * http_content);
-
 #include <iostream>
 #include <cstdio>
 #include <cstdint>
 #include <cstring>
 #include <utility>
 #include <cinttypes>
-
 #include <list>
 
 #define namespace std;
 
+void *dialogue(void *args);
+char * get_url(char * http_content);
 
