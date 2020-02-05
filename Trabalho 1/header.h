f2803259a862f45f77662bf8cb6f7efc6e86eb32
201519040032 <dimiortt@gmail.com>
2020-02-05 07:53:07 -0400

    Rename

--- Trabalho 1/header.h
+++ Trabalho 1/header.h
@@ -1,3 +1,23 @@
+//
+// OVERVIEW: Header.h
+// ========
+// This is the Header definition of
+// structures used in the implementation
+// of the ARP Request Algorithm, and 
+// contains:
+//
+// Definition of structures of Interface,
+// Ethernet, IP and ARP Headers.
+// Also the definition of the Hash Table
+// used to store the ARP Cache.
+//
+// Declaration of every function used
+// in the algortithm.
+//
+// Author(s): Dimitre Ortt
+// Last revision: 18/01/2020
+// =========
+
 #include <errno.h>
 #include <stdio.h>
 #include <netdb.h>
