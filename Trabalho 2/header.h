2618c33f31b914d2225de9b94b384f041b21d685
201519040032 <dimiortt@gmail.com>
2020-02-05 07:35:17 -0400

    Rename

---
+++ Trabalho 2/header.h
@@ -0,0 +1,26 @@
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h> 
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <pthread.h>
+#include <netdb.h>
+
+void *dialogue(void *args);
+char * get_url(char * http_content);
+
+#include <iostream>
+#include <cstdio>
+#include <cstdint>
+#include <cstring>
+#include <utility>
+#include <cinttypes>
+
+#include <list>
+
+#define namespace std;
+
+
